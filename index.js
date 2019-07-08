97a195491258b787e6da37b8de8f18a3ced38572
capable-daemon-6664 <hello@isabelklee.co>
2019-07-08 21:07:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var animal = 'dog'
-
-function myAnimal() {
+/// animal challenge
+var animal = 'dog';
+  function myAnimal() {
   return animal
 }
 
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+var yourAnimal = 'cat';
+  function yourAnimal() {
+  return yourAnimal;
 }
 
+/// arithmetic challenge
 function add2(n) {
+  const two = 2;
   return n + two
 
-  // Feel free to move things around!
-  const two = 2
 }
 
+
+/// funky function challenge
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
@@ -26,4 +26,8 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+
+var theFunk = funkyFunction ();
+    return theFunk
+  }
